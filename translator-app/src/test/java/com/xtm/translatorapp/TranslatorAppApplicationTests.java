7b47984710669042dcfee5d5af4e32db21e9df72
kuumuparuri <kuumuparuri@gmail.com>
2021-08-18 13:04:50 +0200

    Added initial repository

---
+++ translator-app/src/test/java/com/xtm/translatorapp/TranslatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtm.translatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
